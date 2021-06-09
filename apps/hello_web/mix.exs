81370646e81803e65b4e1399ee9bbd7fd009fa2e
c4710n <c4710n@users.noreply.github.com>
2021-06-09 11:20:16 +0800

    ydeps: upgrade phoenix to 1.5.8

--- apps/hello_web/mix.exs
+++ apps/hello_web/mix.exs
@@ -37,8 +37,8 @@ defmodule HelloWeb.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.7"},
-      {:phoenix_live_view, "~> 0.15.0"},
+      {:phoenix, "~> 1.5.8"},
+      {:phoenix_live_view, "~> 0.15.1"},
       {:floki, ">= 0.27.0", only: :test},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
