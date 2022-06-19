3578cba724793e2ac5048716a611167934cc54bf
c4710n <c4710n@users.noreply.github.com>
2022-06-20 00:05:19 +0800

    deps: upgrade phoenix to 1.6.9

--- apps/hello_web/mix.exs
+++ apps/hello_web/mix.exs
@@ -37,7 +37,7 @@ defmodule HelloWeb.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.7"},
+      {:phoenix, "~> 1.6.9"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.17.5"},
