eda61fee697d0d3ca2125613ac9ddaa5b5fd39ed
c4710n <c4710n@users.noreply.github.com>
2022-09-22 17:35:13 +0800

    WIP

--- apps/hello_web/assets/tailwind.config.js
+++ apps/hello_web/assets/tailwind.config.js
@@ -1,8 +1,9 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
 
 module.exports = {
-  content: ['../lib/*_web/**/*.*ex', './{global,lib,pages}/**/*.js'],
+  content: ['../lib/*/**/*.*ex', './{global,lib,pages}/**/*.js'],
   theme: {
     extend: {
       fontFamily: {
@@ -23,5 +24,14 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/line-clamp'),
+    plugin(({ addVariant }) => {
+      addVariant('phx-loading', ['.phx-loading&', '.phx-loading &'])
+      addVariant('phx-connected', ['.phx-connected&', '.phx-connected &'])
+      addVariant('phx-error', ['.phx-error&', '.phx-error &'])
+      addVariant('phx-no-feedback', ['.phx-no-feedback&', '.phx-no-feedback &'])
+      addVariant('phx-click-loading', ['.phx-click-loading&', '.phx-click-loading &'])
+      addVariant('phx-submit-loading', ['.phx-submit-loading&', '.phx-submit-loading &'])
+      addVariant('phx-change-loading', ['.phx-change-loading&', '.phx-change-loading &'])
+    }),
   ],
 }
