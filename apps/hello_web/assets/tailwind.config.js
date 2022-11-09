f3a304666ff45c2aa88ce10c1b8347fc48777869
c4710n <c4710n@users.noreply.github.com>
2022-11-09 11:35:03 +0800

    feat(assets): add new tailwind variants - phx-loading phx-connected phx-error

--- apps/hello_web/assets/tailwind.config.js
+++ apps/hello_web/assets/tailwind.config.js
@@ -29,6 +29,9 @@ module.exports = {
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/line-clamp'),
     plugin(({ addVariant }) => {
+      addVariant('phx-loading', ['.phx-loading&', '.phx-loading &'])
+      addVariant('phx-connected', ['.phx-connected&', '.phx-connected &'])
+      addVariant('phx-error', ['.phx-error&', '.phx-error &'])
       addVariant('phx-no-feedback', ['.phx-no-feedback&', '.phx-no-feedback &'])
       addVariant('phx-click-loading', ['.phx-click-loading&', '.phx-click-loading &'])
       addVariant('phx-submit-loading', ['.phx-submit-loading&', '.phx-submit-loading &'])
