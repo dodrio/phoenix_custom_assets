649981a762635768ed907c94fae7aed276da02b3
c4710n <c4710n@users.noreply.github.com>
2023-02-04 19:36:20 +0800

    feat(assets): remove alpinejs

--- apps/hello_web/assets/index.js
+++ apps/hello_web/assets/index.js
@@ -4,7 +4,6 @@ import 'vite/modulepreload-polyfill'
 import '@fontsource/inter/variable.css'
 import '@fontsource/jetbrains-mono/variable.css'
 
-import './lib/alpine'
-import './lib/tailwind'
 import './lib/phoenix'
 import './lib/phoenix_live_view'
+import './lib/tailwind'
--- apps/hello_web/assets/lib/alpine/index.js
+++
@@ -1,6 +0,0 @@
-import Alpine from 'alpinejs'
-import './style.css'
-
-window.Alpine = Alpine
-
-Alpine.start()
