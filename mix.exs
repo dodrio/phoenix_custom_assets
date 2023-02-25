34a424c6765aa3d3a64d1e455b3f3c70bea8d0dc
c4710n <c4710n@users.noreply.github.com>
2023-02-25 13:03:11 +0800

    deps: upgrade phoenix and phoenix_live_view

--- mix.exs
+++ mix.exs
@@ -32,10 +32,10 @@ defmodule Hello.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.0-rc.3", override: true},
+      {:phoenix, "~> 1.7.0"},
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.18.14"},
+      {:phoenix_live_view, "~> 0.18.16"},
       {:heroicons, "~> 0.5"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.7.2"},
