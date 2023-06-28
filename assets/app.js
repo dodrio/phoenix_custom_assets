57126fd945090586847a2eeacb541ce2ff29f137
c4710n <c4710n@users.noreply.github.com>
2023-06-29 00:23:53 +0800

    fix(assets): fix typo

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
 import '@fontsource-variable/inter'
-import '@fontsource-valiable/jetbrains-mono'
+import '@fontsource-variable/jetbrains-mono'
 
 import './lib/phoenix'
 import './lib/phoenix_live_view'
