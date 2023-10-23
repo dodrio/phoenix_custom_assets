1d69529f564b0bb673d4b1239abd601dc8dfb808
c4710n <c4710n@users.noreply.github.com>
2023-10-23 09:19:23 +0800

    feat: upgrade to phoenix 1.7.7

--- config/prod.exs
+++ config/prod.exs
@@ -10,6 +10,9 @@ config :hello, HelloWeb.Endpoint, cache_static_manifest: "priv/static/cache_mani
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Hello.Finch
 
+# Disable Swoosh Local Memory Storage
+config :swoosh, local: false
+
 # Do not print debug messages in production
 config :logger, level: :info
 
--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Hello.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "1.7.6"},
+      {:phoenix, "1.7.7"},
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.19.0"},
